8a1e6d4c6544b2412211ac5699c6e07b83d309ee
Mileto Di Marco <45310927+Mileto75@users.noreply.github.com>
2021-03-09 16:43:05 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,10 @@
 "use strict";
 
-var txtPrijsExcl, txtBtw, divFeedBack;
-
 window.addEventListener('load', Initieer);
 
-function Initieer() {
-  KoppelElementen();
-  ToonStandaardwaarden();
-  let btnBereken = document.querySelector("#btnBereken");
-  btnBereken.addEventListener("click", ToonBerekeningPrijsIncl);
+function Initialize()
+{
+  
 }
 
-function KoppelElementen() {
-  txtPrijsExcl = document.getElementById("txtPrijsExcl");
-  txtBtw = document.getElementById("txtBtw");
-  divFeedBack = document.getElementById("divFeedBack");
-}
 
-function ToonStandaardwaarden() {
-  txtPrijsExcl.value = '100';
-  txtBtw.value = '6';
-}
-
-function ToonBerekeningPrijsIncl() {
-
-}
-
-function BerekenDeBtw() {
-
-}
